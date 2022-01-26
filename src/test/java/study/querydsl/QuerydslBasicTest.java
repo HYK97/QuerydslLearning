565d1c50d7c8a92aec3dfc36291730f3c2a9e4f9
ddh963963 <ddh963963@naver.com>
2022-01-26 17:59:43 +0900

    Test: 페이징 테스트 추가
    QuerydslBasicTest에 페이징 테스트를 추가했음
    페이징은 fetchResults를 사용
    복잡한페이징에는 사용하지말자

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -169,4 +169,29 @@ public class QuerydslBasicTest {
 
 
     }
+
+    @Test
+    public void paging1() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)//몇번째 부터 끊어서 (N부터시작)
+                .limit(2) //몇개씩 가져올까?
+                .fetch();
+        assertThat(result.size()).isEqualTo(2);
+    }
+    @Test
+    public void paging2() {
+        QueryResults<Member> queryResults = queryFactory.selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)//몇번째 부터 끊어서 (N부터시작)
+                .limit(2) //몇개씩 가져올까?
+                .fetchResults();
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
+
+
 }
