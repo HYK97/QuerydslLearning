556aaf5d43889b6e51c44aac69f6637610d5c6b6
ddh963963 <ddh963963@naver.com>
2022-01-26 17:41:08 +0900

    Test: 정렬테스트를 추가
    QuerydslBasicTest에 정렬 테스트를 추가했음

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,10 +1,12 @@
 package study.querydsl;
 
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,15 +35,15 @@ public class QuerydslBasicTest {
     public void testEntity() {
         queryFactory = new JPAQueryFactory(em);
 
-        Team teamA =new Team("teamA");
-        Team teamB =new Team("teamB");
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
         em.persist(teamA);
         em.persist(teamB);
 
-        Member member1 =new Member("member1",10,teamA);
-        Member member2 =new Member("member2",20,teamA);
-        Member member3 =new Member("member3",30,teamA);
-        Member member4 =new Member("member4",40,teamA);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamA);
+        Member member4 = new Member("member4", 40, teamA);
 
         em.persist(member1);
         em.persist(member2);
@@ -95,16 +97,76 @@ public class QuerydslBasicTest {
         assertThat(findmember.getUsername()).isEqualTo("member1");
 
     }
+
     @Test
     public void searchAndParam() {
         Member findmember = queryFactory
                 .selectFrom(member)
                 .where(
                         member.username.eq("member1")
-                        ,(member.age.eq(10)))
+                        , (member.age.eq(10)))
                 .fetchOne();
 
         assertThat(findmember.getUsername()).isEqualTo("member1");
+
+    }
+
+
+    @Test
+    public void resultFetch() {
+        //List
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+        //단 건
+        Member findMember1 = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+        //처음 한 건 조회
+        Member findMember2 = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+        //페이징에서 사용
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+        results.getTotal();
+        List<Member> results1 = results.getResults();
+
+
+        //count 쿼리로 변경
+        long count = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+    }
+
+    /**
+     * 회원 정렬 순서
+     * 1.회원 나이 내림차순
+     * 2.회원 이름 올림차순
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+    @Test
+    public void sort() {
+        em.persist(new Member(null,100));
+        em.persist(new Member("member5",100));
+        em.persist(new Member("member6",100));
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(
+                        member.age.desc()
+                        , member.username.asc().nullsLast()
+                )
+                .fetch();
+        Member member5= result.get(0);
+        Member member6= result.get(1);
+        Member memberNull= result.get(2);
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+
 
     }
 }
