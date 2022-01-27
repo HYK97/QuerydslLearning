3bc1fdfb889dd28ec4b308b1d25e5bc6367a5cdc
ddh963963 <ddh963963@naver.com>
2022-01-27 21:44:46 +0900

    Test(QuerydslBasicTest): joinTest 추가
    outer 조인과 theta 조인의 다른점
    outer 조인은 join절에(테이블.연관관계,테이블)
    theta 조인은 from절에(테이블,테이블)처럼 연관관계가없음

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -243,4 +243,49 @@ public class QuerydslBasicTest {
 
     }
 
+    /**
+     * 팀 A 에 소속된 모든 회원
+     * */
+    @Test
+    public void join() throws Exception{
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team) //left right inner 조인은 member.team , team 과같이 연관관계가 있는것만 조인함
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result)
+                .extracting("username") //해당 열이름
+                .containsExactly("member1","member2");//
+
+
+    }
+
+    /**
+     * 세타 조인(연관관계가 없는 필드로 조인)
+     * 회원의 이름이 팀 이름과 같은 회원 조회
+     */
+
+    @Test
+    public void theta_join() throws Exception {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)  //select * from team, member where member.username = team.name
+                .where(member.username.eq(team.name))
+                .fetch();
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+
+        assertThat(result.size()).isEqualTo(2);
+
+
+    }
+
 }
