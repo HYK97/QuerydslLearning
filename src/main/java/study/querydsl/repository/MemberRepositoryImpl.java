ac0b0a594ea5edc99a59bab027a9b3191b72986f
ddh963963 <ddh963963@naver.com>
2022-02-04 00:32:31 +0900

    Test ,feat : page 기능 추가
    MemberRepositoryImpl.java : simplepage , complexpagef를 각각추가
    
    MemberRepositoryTest.java : 페이지 테스츠 추가

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -1,19 +1,24 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
 
 import javax.persistence.EntityManager;
+
 import java.util.List;
 
 import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.Entitiy.QMember.member;
 import static study.querydsl.Entitiy.QTeam.team;
-
+import static org.springframework.data.domain.Pageable.*;
 
 //명명 규칙 중요 Impl 이라고해야댐
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
@@ -45,6 +50,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                 .fetch();
     }
 
+
     private BooleanExpression ageLoeEq(Integer ageLoe) {
         return ageLoe!=null ? member.age.loe(ageLoe) : null;
     }
@@ -60,4 +66,91 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
     private BooleanExpression usernameEq(String username) {
         return hasText(username) ? member.username.eq(username) : null;
     }
+
+
+    // Paging Query---------------------------
+
+    @Override //쉽거나 데이터가 적을때는이런식으로 하자.
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) { //몇페이지 조회할지 알려주는 매개변수
+        QueryResults<MemberTeamDto> results = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId")
+                        , member.username
+                        , member.age
+                        , team.id.as("teamId")
+                        , team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername())
+                        , teamNameEq(condition.getTeamName())
+                        , ageGoeEq(condition.getAgeGoe())
+                        , ageLoeEq(condition.getAgeLoe()))
+                .offset(pageable.getOffset()) //오프셋 리미트 설정해줌
+                .limit(pageable.getPageSize())
+                .fetchResults();// 꼭 results 로 해야함.
+
+        List<MemberTeamDto> content = results.getResults();
+        long total = results.getTotal();
+
+        return new PageImpl<>(content,pageable,total);
+    }
+
+
+
+    //복잡한구현 두개 따로 하는경우 -> 카운트 쿼리가 컨텐츠 쿼리보다 쉬울때. 복잡한 쿼리 두방을 날리기보단 한개는 쉽게날리면 성능 최적화 시킬수있음
+    @Override
+    public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+
+        List<MemberTeamDto> content = getMemberTeamDtos(condition, pageable);
+          // 예를 들어 카운트 먼저날리고 없으면 안날릴때 와 같이 최적화함. 왠만하면 카운터 최적화하자 데이터 많을때
+        long total = getTotal(condition);
+
+        return new PageImpl<>(content,pageable,total);
+
+    }
+
+    //이런식으로 리팩토링해서써도된다.
+    private long getTotal(MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId")
+                        , member.username
+                        , member.age
+                        , team.id.as("teamId")
+                        , team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername())
+                        , teamNameEq(condition.getTeamName())
+                        , ageGoeEq(condition.getAgeGoe())
+                        , ageLoeEq(condition.getAgeLoe()))
+                .fetchCount();
+    }
+
+    private List<MemberTeamDto> getMemberTeamDtos(MemberSearchCondition condition, Pageable pageable) {
+        List<MemberTeamDto> content = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId")
+                        , member.username
+                        , member.age
+                        , team.id.as("teamId")
+                        , team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername())
+                        , teamNameEq(condition.getTeamName())
+                        , ageGoeEq(condition.getAgeGoe())
+                        , ageLoeEq(condition.getAgeLoe()))
+                .offset(pageable.getOffset()) //오프셋 리미트 설정해줌
+                .limit(pageable.getPageSize())
+                .fetch();// 꼭 results 로 해야함.
+        return content;
+    }
+
 }
--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import study.querydsl.Entitiy.Member;
 import study.querydsl.Entitiy.Team;
 import study.querydsl.dto.MemberSearchCondition;
@@ -12,8 +14,10 @@ import study.querydsl.dto.MemberTeamDto;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+import java.nio.file.Path;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -77,8 +81,39 @@ class MemberRepositoryTest {
         List<MemberTeamDto> result = memberRepository.search(condition);
 
         assertThat(result).extracting("username").containsExactly("member4");
-
-
     }
+
+    @Test
+    public void searchPageSimple() throws Exception{
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition =new MemberSearchCondition();
+        PageRequest pageRequest = PageRequest.of(0, 3);
+
+
+
+        Page<MemberTeamDto> result = memberRepository.searchPageSimple(condition,pageRequest);
+
+        assertThat(result.getSize()).isEqualTo(3);
+        assertThat(result.getContent()).extracting("username").containsExactly("member1","member2","member3");
+    }
+
+
+
+
 
 }
